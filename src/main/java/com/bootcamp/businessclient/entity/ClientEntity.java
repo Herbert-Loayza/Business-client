cd84fb29098cfdf907aed148c101d2249a77e9c4
Herbert-Loayza <loayza.herbert@gmail.com>
2022-03-10 20:34:20 -0500

    client_mongo

--- src/main/java/com/bootcamp/businessclient/entity/ClientEntity.java
+++ src/main/java/com/bootcamp/businessclient/entity/ClientEntity.java
@@ -2,11 +2,14 @@ package com.bootcamp.businessclient.entity;
 
 import lombok.Data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(value = "clients")
 public class ClientEntity {
 
-
+    @Id
     private String id;
     private String name;
     private String surname;
